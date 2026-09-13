cc2725de47ed1bc33f899a2a2d227b705df677d9
Bryce Klinker <bryce.klinker@gmail.com>
2026-09-13 14:11:36 -0500

    feat: refactoring tests to make tests simpler

--- red-rising/tests/player_movement_tests.rs
+++ red-rising/tests/player_movement_tests.rs
@@ -7,23 +7,27 @@ mod player_movement_tests {
     fn setup_testing_app() -> App {
         let mut app = App::new();
         app.add_plugins(MinimalPlugins);
+        app.add_plugins(player_movement_plugin);
         app.insert_resource(ButtonInput::<KeyCode>::default());
+        app.update();
         return app;
     }
 
     #[test]
     fn when_w_is_pressed_then_player_moves_forward() {
         let mut app = setup_testing_app();
-        app.add_plugins(player_movement_plugin);
-        app.update();
 
-        app.world_mut()
-            .resource_mut::<ButtonInput<KeyCode>>()
-            .press(KeyCode::KeyW);
-        app.update();
+        press_key(& mut app, KeyCode::KeyW);
 
         let transform = app.world_mut().query::<&Transform>().single(app.world()).unwrap();
         assert_eq!(transform.translation.x, 0.0);
         assert_eq!(transform.translation.y, 1.0);
+    }
+
+    fn press_key(app: &mut App, key: KeyCode) {
+        app.world_mut()
+            .resource_mut::<ButtonInput<KeyCode>>()
+            .press(key);
+        app.update();
     }
 }
